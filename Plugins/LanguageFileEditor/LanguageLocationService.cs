a2bb0fcbb4ad2e11549993f1ad2c3534fe05cf43
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2014-04-09 10:49:23 +0300

    support physicalPath configs

--- Plugins/LanguageFileEditor/LanguageLocationService.cs
+++ Plugins/LanguageFileEditor/LanguageLocationService.cs
@@ -15,12 +15,24 @@ namespace EPiServer.Plugins.LanguageFileEditor
         public static string LanguagePath()
         {
             var providers = EPiServer.Framework.Configuration.EPiServerFrameworkSection.Instance.Localization.Providers;
-            var path = providers.Cast<ProviderSettings>()
+            var providerSettings = providers.Cast<ProviderSettings>().ToList();
+            var physicalPaths = providerSettings
+                .Select(settings => settings.Parameters)
+                .Select(parameters => parameters.Get("physicalPath"))
+                .Where(p => !p.IsNullOrEmpty())
+                .ToList();
+
+            if (physicalPaths.Any())
+            {
+                return physicalPaths.FirstOrDefault();
+            }
+
+            var virtualPaths = providerSettings
                 .Select(settings => settings.Parameters)
                 .Select(parameters => parameters.Get("virtualPath"))
                 .FirstOrDefault(vp => !vp.IsNullOrEmpty());
 
-            return HttpContext.Current.Server.MapPath(path);
+            return HttpContext.Current.Server.MapPath(virtualPaths);
         }
     }
 }
